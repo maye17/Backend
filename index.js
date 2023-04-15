fc648377702f03121fd1b29e1e201fab99528cae
maye17 <mayerlin.dossantos@gmail.com>
2023-04-15 10:45:49 -0300

    modificando consulta del metodo getProductById

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ class ProductManager {
 
     addProduct(product){
        
+       
         let valueCode = this.products.find((article) => article.code === product.code);
-        if(valueCode){
-            return 'This code already exists';
-        
+
+        if(!valueCode){
+            if(product.title && product.description && product.price && product.thumbnail && product.code){
+              let newProduct={...product, id: this.id}
+                this.products.push(newProduct);
+                 this.id ++;
+
+                return 'product  added in my list'             
+            
+            } else {
+                return 'Fields missing'
+            }            
+        }else {
+            return 'This code already exists'
         }
-        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code){
-             return 'Fields missing';
-        }
-         let newProduct;
-        newProduct={...product, id: this.id}
-        this.products.push(newProduct);
-        this.id ++;
-                return'product added';
+       
     }
 
  
@@ -32,7 +37,7 @@ class ProductManager {
     getProductById(id){
 
         let searchId = this.products.find((item)=> item.id === id)
-        if(!searchId){
+        if(searchId){
            return searchId
         }else {
             console.log("Not found");
@@ -41,17 +46,18 @@ class ProductManager {
 }
 
 const product = {
+    title:'Camisa De Hombre Slim Fit',
+    description: 'Camisa De Hombre Slim Fit, Cuello camisero abotonado, Ajustado, Punta de puntilla de algodón premium, Botones de nácar, Insignia de cocodrilo verde en el pecho, Cotton (100%)',
+    price:40.000,
+    thumbnail:'https://www.lacoste.com/ar/lacoste/hombre/ropa/camisas/camisa-de-hombre-slim-fit/3666165451391.html?color=T01&gclid=Cj0KCQjwlumhBhClARIsABO6p-ymC4l5Hce_68x4PHdV9xwh8p-e3thi08rWS37P4nOIvnhIQvMzfawaAhnEEALw_wcB',
+    code:'lacoste123',
+    stock: 200,
+  };
 
-    title:'titulos',
-    description:'rpuebas' ,
-    price:10,
-    thumbnail:'sin imagen',
-    code:'3',
-    stock:30,
-};
+
 
 
 const productsManager = new ProductManager();
-const result = productsManager.addProduct(product)
-console.log(result);
-/* console.log(productsManager.getProducts(product)); */
+console.log(productsManager.addProduct(product));
+console.log(productsManager.getProducts());
+console.log(productsManager.getProductById(32));
